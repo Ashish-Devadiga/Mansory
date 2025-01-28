58088b562e5ffcd60735bc58c9f7ff48e175a26a
Ashish Devadiga <ashishdevadiga33@gmail.com>
2025-01-28 12:57:07 +0530

    Update mongoose-connection.js

--- connection/mongoose-connection.js
+++ connection/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const config = require("config")
 const dbgr = require("debug")(`${config.get("NODE_ENV")}:mongoose`)
 
-db = "mongodb+srv://Ashish:aQTcc9dU7xnvneA2@mansory.gvprq.mongodb.net/?retryWrites=true&w=majority&appName=Mansory"
+DB = "mongodb+srv://Ashish:aQTcc9dU7xnvneA2@mansory.gvprq.mongodb.net/?retryWrites=true&w=majority&appName=Mansory"
 mongoose.connect(DB)
   .then(function(){
     dbgr("conected");
